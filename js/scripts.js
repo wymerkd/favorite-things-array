01604c701c0840d1cfaa8b2ea1cd1ea3d810d498
Student <example@example.com>
2019-10-28 09:30:58 -0700

    add list of favorite things created from user input using an array.

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('#fav-things').submit(function(event) {
+    event.preventDefault();
+    var favThing1 = $('#favorite-thing1').val();
+    var favThing2 = $('#favorite-thing2').val();
+    var favThing3 = $('#favorite-thing3').val();
+    var favThing4 = $('#favorite-thing4').val();
+    var favThings = [favThing1, favThing2, favThing3, favThing4];
+    var bestFavThings = [];
+    bestFavThings.push(favThings[1], favThings[0], favThings[2]);
+    console.log(bestFavThings);
+    $("ul#fav-list").append("<li>" + bestFavThings[0] + "</li>" + "<li>" + bestFavThings[1] + "</li>" + "<li>" + bestFavThings[2] + "</li>");
+
+  });
+});
